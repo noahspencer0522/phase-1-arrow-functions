5bc2293224bef929b54090ef94eac383ddcd6678
Noah Spencer <noahspencer0522@gmail.com>
2021-09-08 11:34:30 -0500

    completed assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+ function divide(){
+     return 2000 / 100 
+ }
+
+ const square = x => x*x
+ const add = (p1,p2) => {
+     console.log(`Adding ${p1}`)
+     console.log(`Adding ${p2}`)
+     return p1 + p2
+ }
